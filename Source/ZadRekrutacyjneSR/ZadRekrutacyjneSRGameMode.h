10c56cace334c644ea1d00f37211ea4f1c00987e
KingParadox <tornado4d@gmail.com>
2024-03-18 19:54:21 +0100

    ProjectUpload

---
+++ Source/ZadRekrutacyjneSR/ZadRekrutacyjneSRGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZadRekrutacyjneSRGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AZadRekrutacyjneSRGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AZadRekrutacyjneSRGameMode();
+};
+
+
+
